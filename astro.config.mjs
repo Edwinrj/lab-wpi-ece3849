39427bf7fae1ce9ad7b93f38fee3fea02c81b4b4
edwinrj <edwin.rios1@utp.ac.pa>
2025-07-10 01:57:54 -0500

    Se  agrega scrooll to top y  giscus

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import starlight from '@astrojs/starlight';
 import decapCmsOauth from "astro-decap-cms-oauth";
 
 import vercel from '@astrojs/vercel';
+import starlightScrollToTop from 'starlight-scroll-to-top';
+import starlightGiscus from 'starlight-giscus';
 
 // https://astro.build/config
 export default defineConfig({
@@ -31,7 +33,29 @@ export default defineConfig({
 					autogenerate: { directory: 'reference' },
 				},
 			],
-		}),
+			plugins: [
+				starlightScrollToTop({
+					// Button position
+					position: 'right', // 'left' or 'right'      
+					// Tooltip text
+					tooltipText: 'Back to top',
+					showTooltip: true,
+					// Use smooth scrolling
+					smoothScroll: true,
+					// Visibility threshold (show after scrolling 20% down)
+					threshold: 10,
+				}),
+				starlightGiscus({
+					repo: "Edwinrj/lab-wpi-ece3849",
+					repoId: 'R_kgDOPKCuOw',
+					category: 'Q&A',
+					categoryId: 'DIC_kwDOPKCuO84Cswiv',
+					reactions: true,
+					lazy: true
+				})
+			],
+
+		},
 	],
 
 	adapter: vercel({
