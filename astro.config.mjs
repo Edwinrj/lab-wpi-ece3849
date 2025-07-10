53cbac42fe06f1916460854754b6fcaa9b67c8a7
edwinrj <edwin.rios1@utp.ac.pa>
2025-07-10 02:01:31 -0500

    corrección

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,58 +9,49 @@ import starlightGiscus from 'starlight-giscus';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		decapCmsOauth(),
-		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
-			customCss: [
-				'./src/styles/custom.css',
-			],
-			components: {
-				Head: './src/components/head.astro',
-			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-			plugins: [
-				starlightScrollToTop({
-					// Button position
-					position: 'right', // 'left' or 'right'      
-					// Tooltip text
-					tooltipText: 'Back to top',
-					showTooltip: true,
-					// Use smooth scrolling
-					smoothScroll: true,
-					// Visibility threshold (show after scrolling 20% down)
-					threshold: 10,
-				}),
-				starlightGiscus({
-					repo: "Edwinrj/lab-wpi-ece3849",
-					repoId: 'R_kgDOPKCuOw',
-					category: 'Q&A',
-					categoryId: 'DIC_kwDOPKCuO84Cswiv',
-					reactions: true,
-					lazy: true
-				})
-			],
-
+  integrations: [
+	decapCmsOauth(),
+	starlight({
+	  title: 'My Docs',
+	  social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+	  customCss: ['./src/styles/custom.css'],
+	  components: {
+		Head: './src/components/head.astro',
+	  },
+	  sidebar: [
+		{
+		  label: 'Guides',
+		  items: [
+			{ label: 'Example Guide', slug: 'guides/example' },
+		  ],
 		},
-	],
-
-	adapter: vercel({
-		webAnalytics: {
-			enabled: true,
+		{
+		  label: 'Reference',
+		  autogenerate: { directory: 'reference' },
 		},
+	  ],
+	  plugins: [
+		starlightScrollToTop({
+		  position: 'right',
+		  tooltipText: 'Back to top',
+		  showTooltip: true,
+		  smoothScroll: true,
+		  threshold: 10,
+		}),
+		starlightGiscus({
+		  repo: 'Edwinrj/lab-wpi-ece3849',
+		  repoId: 'R_kgDOPKCuOw',
+		  category: 'Q&A',
+		  categoryId: 'DIC_kwDOPKCuO84Cswiv',
+		  reactions: true,
+		  lazy: true,
+		}),
+	  ],
 	}),
+  ],
+  adapter: vercel({
+	webAnalytics: {
+	  enabled: true,
+	},
+  }),
 });
